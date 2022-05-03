97382857c5a87202f2de2103925cac5a0c5d50ce
SpidySamurai <javierchiortiz@gmail.com>
2022-05-03 18:56:59 -0500

    Add routing

--- src/App/index.js
+++ src/App/index.js
@@ -4,10 +4,14 @@ import { PokeList } from "../components/PokeList";
 import { PokeCard } from "../components/PokeCard";
 import { PokeSearch } from "../components/PokeSearch";
 import { usePokedex } from "./usePokedex";
+import PokeDescription from "../components/PokeDescription";
+
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 function App() {
   const { loading, searchValue, setSearchValue, searchedPokemons } =
     usePokedex();
+  const [selectedPokemon, setSelectedPokemon] = React.useState({});
   return (
     <React.Fragment>
       <PokeHeader>
@@ -17,17 +21,26 @@ function App() {
         ></PokeSearch>
       </PokeHeader>
       <main>
-        <PokeList loading={loading} searchedPokemons={searchedPokemons}>
-          {(pokemon) => (
-            <PokeCard
-              key={pokemon.id}
-              pokeNumber={pokemon.id}
-              pokeName={pokemon.name}
-              pokeImg={pokemon.sprites.front_default}
-              pokeType={pokemon.types}
-            />
-          )}
-        </PokeList>
+        <Router>
+          <Route path="/" exact>
+            <PokeList loading={loading} searchedPokemons={searchedPokemons}>
+              {(pokemon) => (
+                <PokeCard
+                  key={pokemon.id}
+                  pokeNumber={pokemon.id}
+                  pokeName={pokemon.name}
+                  pokeImg={pokemon.sprites.front_default}
+                  pokeType={pokemon.types}
+                  pokemon={pokemon}
+                  setPokemon={setSelectedPokemon}
+                />
+              )}
+            </PokeList>
+          </Route>
+          <Route path="/pokemon/:name" exact>
+            <PokeDescription pokemon={selectedPokemon} />
+          </Route>
+        </Router>
       </main>
     </React.Fragment>
   );
