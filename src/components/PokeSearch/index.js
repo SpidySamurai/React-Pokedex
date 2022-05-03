5ed2b80eb0a7d0d8f358d4565c91ec78de855c49
SpidySamurai <javierchiortiz@gmail.com>
2022-05-02 23:16:41 -0500

    Adding input value and search pokemons:

--- src/components/PokeSearch/index.js
+++ src/components/PokeSearch/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./PokeSearch.scss";
 
 function PokeSearch(props) {
+  const onSearchValueChange = (event) => {
+    props.setSearchValue(event.target.value);
+  };
   return (
     <div className="PokeSearch">
-      <input placeholder="Busca un pokemon" />
+      <input
+        placeholder="Busca un pokemon"
+        value={props.searchValue}
+        onChange={onSearchValueChange}
+      />
     </div>
   );
 }
