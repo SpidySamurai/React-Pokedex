ce1b33dd6feb4c2632e0a93d940ef4de51952783
SpidySamurai <javierchiortiz@gmail.com>
2022-05-02 23:23:06 -0500

    Add dinamic colors for types

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -7,17 +7,19 @@ function PokeCard(props) {
       <h2 className="Pokemon-number">N.{props.pokeNumber}</h2>
       <figure>
         <a href="#">
-          <img src={props.pokeImg} alt={props.pokeName}></img>
+          <img src={props.pokeImg} alt={props.pokeName} loading="lazy"></img>
         </a>
       </figure>
       <h2 className="Pokemon-name">{props.pokeName}</h2>
       <div className="Poketypes">
-        <div className="main-type type">
-          <p>{props.pokeMainType}</p>
+        <div className={`type ${props.pokeType[0].type.name}`}>
+          <p>{props.pokeType[0].type.name}</p>
         </div>
-        <div className="sub-type type">
-          <p>{props.pokeSubType}</p>
-        </div>
+        {props.pokeType[1] && (
+          <div className={`type ${props.pokeType[1].type.name}`}>
+            <p>{props.pokeType[1].type.name}</p>
+          </div>
+        )}
       </div>
     </li>
   );
